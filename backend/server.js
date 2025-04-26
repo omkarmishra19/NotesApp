46480578b2972b9418888ab7b032ee5307dbf4f1
gauravjha0711 <gauravjha0711@gmail.com>
2025-04-26 09:34:58 +0530

    Docker added

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+
 // server.js
 const express = require("express");
 const mongoose = require("mongoose");
@@ -21,7 +22,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/notes", noteRoutes);
 
 mongoose
-  .connect("mongodb://127.0.0.1:27017/notespad")
+  .connect("mongodb://db:27017/notespad")
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
@@ -29,5 +30,5 @@ app.get("/health", (req, res) => {
   res.status(200).send("OK");
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
